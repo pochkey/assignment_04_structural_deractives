60e251c34dca6c03b34b0de979092a43b214b869
pochkey <nilesh.wyn@live.com>
2023-10-06 11:18:19 -0400

    ass04

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assignment_04_structural_deractives';
+  number_array: number[]=[10,11,12,13,14,15,16,17,18,19,20]
 }
